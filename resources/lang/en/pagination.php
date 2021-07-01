f400c1135197ac2c800dc7cb85ca5d996f4e3d22
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-07-01 18:37:19 +0200

    feat: add tailwindcss presets and fortify some edits

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next' => 'Next &raquo;',
-
+    'previous'  => '&laquo; Previous',
+    'next'      => 'Next &raquo;',
+    'goto_page' => 'Goto page #:page',
 ];
